ae42a0a37fc3b1c11bd325bec98bababd6150571
Thiago loure <thiagoloure.developer@gmail.com>
2023-02-13 14:35:33 -0300

    Implement Category in memory

---
+++ src/test/java/com/louredev/projweb/ProjwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.louredev.projweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
